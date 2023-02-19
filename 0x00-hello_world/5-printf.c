a17cc3bac070f8a893e32ffc467acbb5229be701
Judiekoech <judiekoech98@gmail.com>
2023-02-19 06:39:58 -0800

    hello, world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer but outcome is a piece of art, \n");
+	return (0);
+
+}
